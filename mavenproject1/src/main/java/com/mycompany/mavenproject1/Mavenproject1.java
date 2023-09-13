8ce5c2a78841439196d234329ac453a5ad791b43
Ruben Javier Bacho Alarcon <130557846+RubenJavierBachoAlarcon@users.noreply.github.com>
2023-09-13 13:22:53 +0200

    hola+

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -11,7 +11,7 @@ package com.mycompany.mavenproject1;
 public class Mavenproject1 {
 
     public static void main(String[] args) {
-        System.out.println("Hola mu buenas");
+        System.out.println("Hola mu buenas+");
                 
     }
 }
