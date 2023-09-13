af0937f3a8e89f51cac66a7fc56d78f5fe6a7518
Ruben Javier Bacho Alarcon <130557846+RubenJavierBachoAlarcon@users.noreply.github.com>
2023-09-13 13:21:44 +0200

    Hola mu buenas .java

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -11,7 +11,7 @@ package com.mycompany.mavenproject1;
 public class Mavenproject1 {
 
     public static void main(String[] args) {
-        System.out.println("Hoa");
+        System.out.println("Hola mu buenas");
                 
     }
 }
